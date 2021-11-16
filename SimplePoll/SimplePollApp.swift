fc69419ecd8d6d4146f14018eb70c707ccb43ae4
AyaanMahimwala <ayaanhaider@gmail.com>
2021-11-16 14:53:49 -0600

    Initial Commit

---
+++ SimplePoll/SimplePollApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePollApp.swift
+//  SimplePoll
+//
+//  Created by Ayaan Mahimwala on 11/16/21.
+//
+
+import SwiftUI
+
+@main
+struct SimplePollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
